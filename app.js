6bfd7c19dbfff49ebcf6343d712d2138e6ae1f3f
A.Rohit Varma <arv032000@gmail.com>
2020-08-25 16:53:34 +0530

    Added New files

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var temp = document.querySelector('.temp');
 var desp = document.querySelector('.desp');
 var image = document.querySelector('.small');
 var inputValue=document.querySelector('.inputValue');
+var sunrise=document.querySelector('.sunrise');
+var sunset=document.querySelector('.sunset');
+var cloud=document.querySelector('.clouds');
+var humidity=document.querySelector('.humi');
 
 button.addEventListener('click',function(){
     fetch('https://api.weatherbit.io/v2.0/current?city='+inputValue.value+'&key=977e14550df24f8f8043652a481c08ba')
@@ -14,10 +18,18 @@ button.addEventListener('click',function(){
         var temprature=data["data"][0]["temp"];
         var icon=data["data"][0]["weather"]["icon"];
         var despdata=data["data"][0]["weather"]["description"];
+        var hum=data["data"][0]["rh"];
+        var cld=data["data"][0]["clouds"];
+        var sunr=data["data"][0]["sunrise"];
+        var suns=data["data"][0]["sunset"];
     
         City.innerHTML=CityName;
-        temp.innerHTML="The Temperature here is "+temprature;
-        desp.innerHTML="The Weather here is "+despdata;
+        temp.innerHTML="Temperature:<br>"+temprature+" C";
+        desp.innerHTML="Description:<br>"+despdata;
+        sunrise.innerHTML="Sunrise: "+sunr;
+        sunset.innerHTML="Sunset: "+suns;
+        humidity.innerHTML="Humidity: "+hum+"%";
+        cloud.innerHTML="Cloud: "+cld+"%";
         image.setAttribute("src","https://www.weatherbit.io/static/img/icons/"+icon+".png");
     })
 .catch(Error=>{ 
